b22dfdd1d89a50f82c807ebc11445b380b24ef36
Taco Vollmer <tacovollmer@gmail.com>
2015-05-20 15:35:28 +0200

    added method for updating grid

--- GameOfHive/ViewController.swift
+++ GameOfHive/ViewController.swift
@@ -10,29 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var cells: [HexagonView] = []
+    var timer: NSTimer! = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         
         let height: CGFloat = 50
         let s = height/2
         let width = CGFloat(sqrt(3.0)) * s
       
+        let grid = HexagonGrid(rows: 50, columns: 50)
+        updateGrid(grid, cellWidth: width, cellHeight: height, sideLength: s)
       
-        let hexagonGrid = HexagonGrid(rows: 50, columns: 50)
-      
-        for row in 0..<hexagonGrid.rows {
-          for column in 0..<hexagonGrid.columns {
-            
-            let x = column & 1 == 0 ? (width * CGFloat(row)) : (width * CGFloat(row)) + (width * 0.5)
-            let y = (height - s/2) * CGFloat(column)
-            
-            let hexView = HexagonView(frame: CGRect(x: x, y: y, width: width, height: height))
-            if let hex = hexagonGrid.hexagon(atLocation:Coordinate(row: row, column: column)) {
-                hexView.alive = hex.active
+        timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("tick:"), userInfo: nil, repeats: true)
+        timer.fire()
+    }
+    
+    func updateGrid(grid: HexagonGrid, cellWidth: CGFloat, cellHeight: CGFloat, sideLength: CGFloat) {
+        for row in 0..<grid.rows {
+            for column in 0..<grid.columns {
+                
+                let x = column & 1 == 0 ? (cellWidth * CGFloat(row)) : (cellWidth * CGFloat(row)) + (cellWidth * 0.5)
+                let y = (cellHeight - sideLength/2) * CGFloat(column)
+                
+                let hexView = HexagonView(frame: CGRect(x: x, y: y, width: cellWidth, height: cellHeight))
+                if let hex = grid.hexagon(atLocation:Coordinate(row: row, column: column)) {
+                    hexView.alive = hex.active
+                }
+                view.addSubview(hexView)
             }
-            view.addSubview(hexView)
-          }
         }
+    }
+    
+    
+    func tick(timer: NSTimer) {
+        
+    }
+    
+    deinit {
+        timer.invalidate()
+        timer = nil
     }
 
     override func didReceiveMemoryWarning() {
