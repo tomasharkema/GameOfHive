17ff9bf239f49287d13782108d84eeb1093ff40c
Taco Vollmer <tacovollmer@gmail.com>
2015-05-20 23:59:24 +0200

    uicolor

--- GameOfHive/UIColor.swift
+++ GameOfHive/UIColor.swift
@@ -6,4 +6,15 @@
 //  Copyright (c) 2015 Beetles. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+// amber
+extension UIColor {
+    static var lightAmberColor : UIColor {
+        return UIColor(red: 1.0, green: 0.7490196078, blue: 0, alpha: 1)
+    }
+    
+    static var darkAmberColor : UIColor {
+        return UIColor(red: 1.0, green: 0.4941176471, blue: 0, alpha: 1)
+    }
+}
