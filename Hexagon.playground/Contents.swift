8ef2d59f1afe932992cf130080e490d7b8e2e15c
Tomas Harkema <tomas@harkema.in>
2015-05-20 13:27:44 +0200

    playground

---
+++ Hexagon.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+let width = CGFloat(50.0)
+let height = CGFloat(50.0)
+
+let s = height / sqrt(3)
+let b = width / 2.0
+let a = sqrt((s*s) - (b*b))
+
